94a2dabcb005ed49142d2d6404791a6ea7f29c64
onnik <onni.kivinen@gmail.com>
2025-04-13 09:23:30 +0300

    Enhance getRestaurantInfo function to support multilingual output based on LANGUAGE parameter

--- src/assets/js/components.js
+++ src/assets/js/components.js
@@ -14,18 +14,26 @@ const restaurantMenuItems = (menu) => {
   return html;
 };
 
-const getRestaurantInfo = (restaurant) => {
+const getRestaurantInfo = (restaurant, LANGUAGE) => {
   const {name, address, postalCode, city, phone, company} = restaurant;
 
-  const info = `<article class="restaurantInfo">
-      <h3>${name}</h3>
-      <p><strong>Address:</strong> ${address}</p>
-      <p><strong>Postal Code:</strong> ${postalCode}</p>
-      <p><strong>City:</strong> ${city}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Company:</strong> ${company}</p>
-    </article>`;
-  return info;
+  return LANGUAGE === 'en'
+    ? `<article class="restaurantInfo">
+          <h3>${name}</h3>
+          <p><strong>Address:</strong> ${address}</p>
+          <p><strong>Postal Code:</strong> ${postalCode}</p>
+          <p><strong>City:</strong> ${city}</p>
+          <p><strong>Phone:</strong> ${phone}</p>
+          <p><strong>Company:</strong> ${company}</p>
+       </article>`
+    : `<article class="restaurantInfo">
+          <h3>${name}</h3>
+          <p><strong>Osoite:</strong> ${address}</p>
+          <p><strong>Postinumero:</strong> ${postalCode}</p>
+          <p><strong>Kaupunki:</strong> ${city}</p>
+          <p><strong>Puh:</strong> ${phone}</p>
+          <p><strong>Yritys:</strong> ${company}</p>
+       </article>`;
 };
 
 export {restaurantRow, restaurantMenuItems, getRestaurantInfo};
