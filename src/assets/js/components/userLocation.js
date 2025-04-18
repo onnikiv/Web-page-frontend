0f95ddc70223ee42cafefec7d468edaee93afe15
onnik <onni.kivinen@gmail.com>
2025-04-18 15:26:58 +0300

    Add userLocation.js for geolocation and nearest restaurant functionality

---
+++ src/assets/js/components/userLocation.js
@@ -0,0 +1,61 @@
+import {distance} from '../utils/euclidean.js';
+import {restaurants} from '../components/restaurants.js';
+import getLanguage from '../utils/language.js';
+import {openRestaurantByClick} from '../map.js';
+import {infoModal} from '../utils/infoModal.js';
+
+const success = (position) => {
+  const coordsObject = position.coords;
+  const userLocation = [coordsObject.longitude, coordsObject.latitude];
+  console.log(userLocation, ' userLocation');
+
+  let closestRestaurant = null;
+  let shortestDistance = Infinity;
+
+  restaurants.forEach((restaurant) => {
+    const dist = distance(userLocation, restaurant.location.coordinates);
+    if (dist < shortestDistance) {
+      shortestDistance = dist;
+      closestRestaurant = restaurant;
+    }
+  });
+
+  if (closestRestaurant) {
+    console.log(
+      `Closest restaurant is ${closestRestaurant.name} at a distance of ${shortestDistance}`
+    );
+    openRestaurantByClick(closestRestaurant);
+  } else {
+    console.log('No restaurants found.');
+  }
+};
+
+const options = {
+  maximumAge: 0,
+  timeout: 5000,
+  enableHighAccuracy: true,
+};
+
+const error = (err) => {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+  const info =
+    getLanguage() === 'fi'
+      ? `Selaimen sijainti on estetty. Salli sijainti selaimen asetuksista.`
+      : `Location access is blocked. Please enable location access in your browser settings.`;
+  infoModal(info);
+};
+
+export const createNearestButton = () => {
+  const nearestButton = document.getElementById('nearest-restaurant');
+
+  nearestButton.innerText = getLanguage() === 'fi' ? `Lähin Ravintola` : `Closest Restaurant`;
+
+  nearestButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    findClosestRestaurant();
+  });
+};
+
+const findClosestRestaurant = () => {
+  navigator.geolocation.getCurrentPosition(success, error, options);
+};
