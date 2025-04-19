f1b15289f70cecd06481a25d9be8412234c260a9
onnik <onni.kivinen@gmail.com>
2025-04-19 19:34:45 +0300

    Add createFavouriteSection and favouriteButtonEvent functions for favorite feature

---
+++ src/assets/js/components/setFavourite.js
@@ -0,0 +1,24 @@
+import getLanguage from '../utils/language.js';
+import {isLoggedIn} from '../utils/logged.js';
+const createFavouriteSection = () => {
+  return `<div class="favourite-section">
+            <p><strong>${
+              getLanguage() === 'fi' ? 'Aseta suosikiksi' : 'Set as Favourite'
+            }</strong></p><button id="favourite-btn">❤️</button>
+          </div>`;
+};
+
+const favouriteButtonEvent = () => {
+  const favouriteSection = document.querySelector('.favourite-section');
+  const favouriteBtnElement = document.getElementById('favourite-btn');
+  favouriteBtnElement.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('testi');
+  });
+
+  isLoggedIn()
+    ? (favouriteSection.style.display = 'grid')
+    : (favouriteSection.style.display = 'none');
+};
+
+export {createFavouriteSection, favouriteButtonEvent};
