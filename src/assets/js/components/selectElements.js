1e98fe0ef58e7736978bc27b4f1c4b8b2ec58afb
onnik <onni.kivinen@gmail.com>
2025-04-19 23:36:02 +0300

    Add favourites filtering functionality and improve select element population

--- src/assets/js/components/selectElements.js
+++ src/assets/js/components/selectElements.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 import {createRestaurantRows} from '../pages/index.js';
 import {restaurants} from './restaurants.js';
 import getLanguage from '../utils/language.js';
+import {createNearestButton} from './userLocation.js';
+import {getAllFavouriteRestaurants} from './postFavourite.js';
 
 const filterRestaurantsByCompany = (type) => {
   return type === 'All'
@@ -9,6 +12,12 @@ const filterRestaurantsByCompany = (type) => {
 };
 
 const filterRestaurantsByCity = (type) => {
+  return type === 'All'
+    ? restaurants
+    : restaurants.filter((restaurant) => restaurant.city === type);
+};
+
+const filterRestaurantsByFavourites = (type) => {
   return type === 'All'
     ? restaurants
     : restaurants.filter((restaurant) => restaurant.city === type);
@@ -31,19 +40,51 @@ const citySelect = () => {
     createRestaurantRows(filteredRestaurants);
   });
 };
+const favouritesSelect = () => {
+  const favouritesSelect = document.querySelector('#favourites-select');
+  favouritesSelect.addEventListener('change', async (event) => {
+    try {
+      const favouriteRestaurants = await getAllFavouriteRestaurants();
+      const option = event.target.value;
+
+      const filteredRestaurants =
+        option === 'All'
+          ? restaurants
+          : restaurants.filter((restaurant) =>
+              favouriteRestaurants.some((fav) => fav === restaurant._id)
+            );
+
+      if (filteredRestaurants.length > 0) {
+        createRestaurantRows(filteredRestaurants);
+      } else {
+        console.log('No restaurants found.');
+      }
+    } catch (error) {
+      console.log('No restaurants found.');
+    }
+  });
+};
 
 const populateSelectElements = () => {
-  // tää on vähä turha nojoo
+  const language = getLanguage();
   const allCitiesOption = document.getElementById('all-cities');
   const allCompaniesOption = document.getElementById('all-companies');
-  allCompaniesOption.innerText = getLanguage() === 'fi' ? `Yritys` : `Company`;
-  allCitiesOption.innerText = getLanguage() === 'fi' ? `Kaupunki` : `City`;
+  const allFavouritesOption = document.getElementById('all-restaurants');
+  const favouritesOption = document.getElementById('favourites-option');
+
+  allCompaniesOption.innerText = language === 'fi' ? `Yritys` : `Company`;
+  allCitiesOption.innerText = language === 'fi' ? `Kaupunki` : `City`;
+  allFavouritesOption.innerText = language === 'fi' ? `Kaikki` : `All`;
+  favouritesOption.innerText = language === 'fi' ? `Suosikit` : `Favourites`;
 };
 
-export {
-  filterRestaurantsByCompany,
-  filterRestaurantsByCity,
-  companySelect,
-  citySelect,
-  populateSelectElements,
+const createSelectElements = () => {
+  companySelect();
+  citySelect();
+  favouritesSelect();
+  populateSelectElements();
+
+  createNearestButton();
 };
+
+export {createSelectElements, filterRestaurantsByCompany, filterRestaurantsByCity};
